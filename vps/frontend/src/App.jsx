c9412270fdfd14c3812c21841cb3a9285f02098c
litenby <michaelshaffer@protonmail.com>
2025-12-09 03:37:04 -0500

    Removed imagery components.

--- vps/frontend/src/App.jsx
+++ vps/frontend/src/App.jsx
@@ -4,12 +4,9 @@ import StationStatusTable from './components/StationStatusTable';
 import AddInstruction from './components/AddInstruction';
 import InstructionsList from './components/InstructionsList';
 import AudioFileList from './components/AudioFileList'; 
-import SoundOriginLocator from './components/SoundOriginLocator';
 import CreatePlotLocal from './components/CreatePlotLocal';
 import BandwidthDisplay from './components/BandwidthDisplay';  
 import RetrieveAudio from './components/RetrieveAudio';
-import SoundOriginControls from './components/SoundOriginControls';
-import SoundOriginMap from './components/SoundOriginMap';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,7 +15,6 @@ function App() {
     <>
       <div><RetrieveAudio /></div>
       <div><CreatePlotLocal /></div>
-      <div><SoundOriginLocator /></div>
       <div><InstructionsList /></div>
       <div><AudioFileList /></div>
       <div><StationStatusTable /></div>
