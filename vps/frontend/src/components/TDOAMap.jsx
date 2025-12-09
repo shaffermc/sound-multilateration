09daadeb6c6daff4c8dc56771da8df375e983945
litenby <michaelshaffer@protonmail.com>
2025-12-09 10:27:14 -0500

    Removed require() statements since not supported in browser by React.

--- vps/frontend/src/components/TDOAMap.jsx
+++ vps/frontend/src/components/TDOAMap.jsx
@@ -3,11 +3,17 @@ import { MapContainer, TileLayer, Marker, Polyline, Popup } from "react-leaflet"
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
+// Proper ES module Leaflet icon imports
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet’s default icon path resolution for React
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
 });
 
 export default function TDOAMap({ result }) {
@@ -18,7 +24,7 @@ export default function TDOAMap({ result }) {
   const mapCenter = [stations[0].lat, stations[0].lon];
 
   return (
-    <MapContainer center={mapCenter} zoom={17} style={{ height: "80vh", width: "100%" }}>
+    <MapContainer center={mapCenter} zoom={17} style={{ height: "100%", width: "100%" }}>
       <TileLayer
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
       />
@@ -26,14 +32,14 @@ export default function TDOAMap({ result }) {
       {/* Stations */}
       {stations.map((s, i) => (
         <Marker key={i} position={[s.lat, s.lon]}>
-          <Popup>Station {i+1}</Popup>
+          <Popup>Station {i + 1}</Popup>
         </Marker>
       ))}
 
       {/* Omit-one solutions */}
       {omit_solutions.map((p, i) => (
         <Marker key={`omit-${i}`} position={[p.lat, p.lon]}>
-          <Popup>Solution {i+1}</Popup>
+          <Popup>Solution {i + 1}</Popup>
         </Marker>
       ))}
 
@@ -42,11 +48,11 @@ export default function TDOAMap({ result }) {
         <Popup>Global Solution</Popup>
       </Marker>
 
-      {/* Hyperbolas */}
+      {/* Hyperbola polylines */}
       {hyperbolas.map((h, idx) => (
         <Polyline
           key={idx}
-          positions={h.points.map(p => [p[0], p[1]])}
+          positions={h.points.map((pt) => [pt[0], pt[1]])}
           color="yellow"
           weight={2}
         />
